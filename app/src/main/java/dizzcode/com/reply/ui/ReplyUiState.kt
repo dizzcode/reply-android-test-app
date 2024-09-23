d7683e71066659b948ffa9a5a4aedc39ff8d0556
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-23 21:57:24 +0530

    Init stage 1 complete

---
+++ app/src/main/java/dizzcode/com/reply/ui/ReplyUiState.kt
@@ -0,0 +1,14 @@
+package dizzcode.com.reply.ui
+
+import dizzcode.com.reply.data.Email
+import dizzcode.com.reply.data.MailboxType
+import dizzcode.com.reply.data.local.LocalEmailsDataProvider
+
+data class ReplyUiState(
+    val mailboxes: Map<MailboxType, List<Email>> = emptyMap(),
+    val currentMailbox: MailboxType = MailboxType.Inbox,
+    val currentSelectedEmail: Email = LocalEmailsDataProvider.defaultEmail,
+    val isShowingHomepage: Boolean = true
+) {
+    val currentMailboxEmails: List<Email> by lazy { mailboxes[currentMailbox]!! }
+}
